4e9266b32855a783259b499b9068606b1ed4155e
iblowu <karina108@rambler.ru>
2019-03-19 15:56:54 +0100

    fixes

--- scripts/zel.js
+++ scripts/zel.js
@@ -28,7 +28,7 @@ function show_grid(e) {
     }
 }
 
-function read_server_file(filePath) {
+function read_file(filePath) {
     var result = null;
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open("GET", filePath);
@@ -39,9 +39,8 @@ function read_server_file(filePath) {
     return result;
 }
 
-function window_to_show(e){
-    if(e.target.textContent==="History")
-    {
+function window_to_show(e) {
+    if (e.target.textContent === "History") {
         grid.setAttribute('style', 'display:none;');
         history.setAttribute('style', 'display:block');
         let continents = read_file("../data/continents.json");
@@ -50,41 +49,44 @@ function window_to_show(e){
 
         console.table(countries);
 
-        var nodes = new vis.DataSet([
-      { id: 1, label: 'Node 1' },
-      { id: 2, label: 'Node 2' },
-      { id: 3, label: 'Node 3' },
-      { id: 4, label: 'Node 4' },
-      { id: 5, label: 'Node 5' }
-        ]);
+        generate_network();
 
-        // create an array with edges
-        var edges = new vis.DataSet([
-            { from: 1, to: 3 },
-            { from: 1, to: 2 },
-            { from: 2, to: 4 },
-            { from: 2, to: 5 }
-        ]);
-
-        // create a network
-
-        // provide the data in the vis format
-        var data = {
-            nodes: nodes,
-            edges: edges
-        };
-        var options = {};
-
-        // initialize your network!
-        var network = new vis.Network(history, data, options);
     }
-
 }
 
 buttons.forEach(function (button) {
     button.addEventListener('click', window_to_show);
-})
+});
 showTopics.addEventListener('click', show_grid);
 
 
 
+function generate_network() {
+    var nodes = new vis.DataSet([
+  { id: 1, label: 'Node 1' },
+  { id: 2, label: 'Node 2' },
+  { id: 3, label: 'Node 3' },
+  { id: 4, label: 'Node 4' },
+  { id: 5, label: 'Node 5' }
+    ]);
+
+    // create an array with edges
+    var edges = new vis.DataSet([
+        { from: 1, to: 3 },
+        { from: 1, to: 2 },
+        { from: 2, to: 4 },
+        { from: 2, to: 5 }
+    ]);
+
+    // create a network
+
+    // provide the data in the vis format
+    var data = {
+        nodes: nodes,
+        edges: edges
+    };
+    var options = {};
+
+    // initialize your network!
+    var network = new vis.Network(history, data, options);
+}
